994eee69b456a294bd549ff1e91059a2366010ff
liyepai <liyejiano@qq.com>
2020-02-29 01:07:51 +0800

    '首页缺个回到底部'

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,8 +2,12 @@
 import request from '../../utils/request.js';
 Page({
   data: {
-    banners: [
-    ]
+    //轮播图
+    banners: [],
+    //分类
+    unlike: [],
+    //楼层数据
+    louceng: []
   },
   onLoad() {
     request({
@@ -13,6 +17,31 @@ Page({
       this.setData({
         banners: message
       })
+    }),
+      request({
+        url: '/home/catitems'
+      }).then(res => {
+        const { message } = res.data
+        const newNav = message.map((v, i) => {
+          //分类
+          if (i === 0) {
+            v.url = '/pages/category/index'
+          }
+          return v
+        })
+        this.setData({
+          unlike: newNav
+        })
+      })
+    request({
+      url: '/home/floordata'
+    }).then(res => {
+     const {message} =res.data
+     console.log(message)
+     this.setData({
+       louceng:message
+     })
+     console.log(this.data.louceng)
     })
   }
 })
