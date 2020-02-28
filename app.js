d98c2152ca5622646ec93f3a7c4e4caf9559aedd
liyepai <liyejiano@qq.com>
2020-02-28 21:33:46 +0800

    '轮播图渲染完毕'

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import request from 'utils/request.js'
 //app.js
 App({
   onLaunch: function () {
- 
+    //配置基准路径
+    request.defaults.baseURL = 'https://api-hmugo-web.itheima.net/api/public/v1'
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,18 @@
+//引入
+import request from '../../utils/request.js';
 Page({
   data: {
-
+    banners: [
+    ]
+  },
+  onLoad() {
+    request({
+      url: '/home/swiperdata'
+    }).then(res => {
+      const { message } = res.data
+      this.setData({
+        banners: message
+      })
+    })
   }
 })
